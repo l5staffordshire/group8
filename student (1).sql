9d0ab8b1b553002fe1754a776c870556e8bdcf94
Akoto-Bamfo Nana Yeboah <akotobamfonana@yahoo.com>
2016-09-02 16:59:38 +0000

    Add files via upload
    data base

---
+++ student (1).sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 02, 2016 at 04:34 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `information`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `StudentID` int(100) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(55) NOT NULL,
+  `LastName` varchar(55) NOT NULL,
+  `DOB` varchar(12) NOT NULL,
+  `ProgrammeLevel` int(9) NOT NULL,
+  `StudentNumber` int(12) NOT NULL,
+  `StudentEmail` varchar(25) NOT NULL,
+  `ResidentialAddress` varchar(55) NOT NULL,
+  `PostalAddress` varchar(55) NOT NULL,
+  `KinFirtsName` varchar(55) NOT NULL,
+  `KinLastName` varchar(55) NOT NULL,
+  `KinNumber` int(12) NOT NULL,
+  `KinEmail` varchar(25) NOT NULL,
+  `KinRelationship` varchar(55) NOT NULL,
+  PRIMARY KEY (`StudentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
